10b04bb5252f494c81ad6d24f96f54eb7da64736
Tarik Kavaz <tarikkavaz@gmail.com>
2021-08-27 14:08:40 +0300

    Button Component

---
+++ src/components/ButtonWithTooltip.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="link" class="has-tooltip">
+    <span class="tooltip"
+      ><div class="tool-arrow"></div>
+      {{tooptip}}
+    </span>
+    <slot/>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String
+    },
+    tooptip: {
+      type: String
+    }
+  }
+}
+</script>
