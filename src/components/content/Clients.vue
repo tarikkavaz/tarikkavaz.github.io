690a3e704bce1542af5591da786d2ef271bd5fa9
Tarik Kavaz <tarikkavaz@gmail.com>
2021-09-23 14:17:54 +0300

    clients

--- src/components/content/Clients.vue
+++ src/components/content/Clients.vue
@@ -1,6 +1,11 @@
 <template>
-  <h3 class="mb-4 text-3xl font-bold">Some Clients</h3>
+  <h3 class="mb-4 text-3xl font-bold">Some Clients <span class="text-xs text-gray-600">(alphabetical)</span></h3>
   <ul class="pl-5 list-disc">
+    <!-- <li>
+      Curious Cluster
+      <span class="tag">Polpo Tech</span>
+      <span class="tag">Curious Games</span>
+    </li> -->
     <li>Emirates</li>
     <li>Flamm Bodrum</li>
     <li>
